3224ba7557e94b7c03cbf98ccbd660592bd4d62c
WonyoungJang <zwonyoung33@gmail.com>
2024-05-06 17:40:26 +0900

    STEP_08_PointLine_

---
+++ STEP_08_접근제한모드/W5_inheritanceField/kor/point/Point_5.java
@@ -0,0 +1,19 @@
+package kor.point;
+
+public class Point_5 {
+    private int x; // x좌표
+    private int y; // y좌표
+
+    public Point_5(int x, int y){ // x와 y 입력받을때의 객체 생성자 Point_5 정의
+        this.x = x;
+        this.y = y;
+    }
+
+    public int get_x(){ // x좌표 반환
+        return x;
+    }
+
+    public int get_y(){ // y좌표 반환
+        return y;
+    }
+}
