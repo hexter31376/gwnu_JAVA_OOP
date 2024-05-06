112bbf65326b2de6383555fa9b49e515fcacbf9e
WonyoungJang <zwonyoung33@gmail.com>
2024-05-06 19:30:05 +0900

    STEP_08_PointLine_7 create

---
+++ STEP_08_접근제한모드/W7_inheritanceClass/Line_7.java
@@ -0,0 +1,50 @@
+import kor.point.*; // 클래스 경로 생략을 위한 import
+
+public class Line_6 extends Point_6 { // point 패키지의 Point_6클래스를 상속받은 Line_6클래스 정의
+    int a, b;
+
+    public Line_6(int x, int y, int a, int b){ // 부모 클래스 필드 x와 y, 자식 클래스(지금 이 클래스) 필드 a, b 입력받을때의 객체 생성자 line_5 정의
+        super(x, y); // this.x = x this.y = y와 동치이나 필드 x와 y는 부모 클래스의 내용이므로 super메소드를 이용해 설정해 주어야 합니다.
+        this.a = a;
+        this.b = b;
+    }
+
+    int get_a(){ // a값 반환
+        if (a < 0){
+            return 0;
+        } else {
+            return a;
+        }
+    }
+
+    int get_b(){ // b값 반환
+        if (b < 0){
+            return 0;
+        } else {
+            return b;
+        }
+    }
+
+    public int get_x() { // x값 반환 // super.public method로 접근한 모습입니다.
+        if (super.get_x() > 0){
+            return super.get_x(); // super.public method로 접근한 모습입니다.
+        } else {
+            return 0;
+        }
+    }
+
+    public int get_y() { // y값 반환 // super.public method로 접근한 모습입니다.
+        if (super.get_y() > 0){
+            return super.get_y(); // super.public method로 접근한 모습입니다.
+        } else {
+            return 0;
+        }
+    }
+
+
+    public double get_dl(){ // 선분의 길이 반환
+        double dl;
+        dl = Math.sqrt(Math.pow(get_x() - a, 2) + Math.pow(get_y() - b, 2)); // 선분의 길이 구하고 dl에 저장 
+        return (dl);
+    }
+}
---
+++ STEP_08_접근제한모드/W7_inheritanceClass/STEP_08_PointLine_7.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+class STEP_08_PointLine_7 {
+    public static void main(String[] args) throws IOException{
+        Line_7 line; // line 패키지의 Line_5 클래스 형태의 객체지시자 line 생성
+        int x, y, a, b; // 사용자에게 입력받을 정수 x, y, a, b 선언
+        double dl; // 선분의 길이 dl
+
+        x = System.in.read() - '0'; // 문자 입력받고 정수화시키기
+        y = System.in.read() - '0'; // 문자 입력받고 정수화시키기
+        a = System.in.read() - '0'; // 문자 입력받고 정수화시키기
+        b = System.in.read() - '0'; // 문자 입력받고 정수화시키기
+
+        line = new Line_7(x, y, a, b); // line 패키지의 Line_5 클래스 형태의 객체를 생성, 초기값으로 x, y, a, b 넘겨주기
+
+        dl = line.get_dl(); // 선분의 길이 저장
+
+        System.out.printf("dl = %f\n", dl); // dl 출력
+    }
+}
+
+// 부모 클래스의 필드가 private로 선언되어 있으면 그 필드로의 접근이 허용되지 않습니다.
+// public으로 되어있는 get_x(), get_y() 메소드로 접근해서 조작해야 합니다.
+// public 접근권한이 걸려있는 get_x(), get_y() 메소드로 접근해서 성공적으로 작동한 모습을 볼 수 있습니다.
