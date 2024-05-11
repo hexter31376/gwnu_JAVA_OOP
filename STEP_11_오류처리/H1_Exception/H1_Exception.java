55e7a94a8ac88e151b9a14aff5b118ccc108e863
WonyoungJang <zwonyoung33@gmail.com>
2024-05-12 05:26:53 +0900

    H1_Exception update

--- STEP_11_오류처리/H1_Exception/H1_Exception.java
+++ STEP_11_오류처리/H1_Exception/H1_Exception.java
@@ -1,43 +1,42 @@
 import java.io.*;
 
-class ZeroException extends Exception {
+class ZeroException extends Exception { //  예외를 상속받은 커스텀 예외
 
 }
 
-class OvfException extends Exception {
+class OvfException extends Exception { //  예외를 상속받은 커스텀 예외
 
 }
 
 class GetN {
-    int get_n() throws IOException, ZeroException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); // 나중에 System.in.read로 바꾸기
-        int n = Integer.parseInt(reader.readLine());
-        if (n == 0) {
-            throw new ZeroException();
+    int get_n() throws IOException, ZeroException { // 이때 여기서 throw해준 예외는 다음 메소드로 넘겨줄 수 있다.
+        int n = System.in.read() - '0'; // 사용자에게 정수 입력받기
+        if (n == 0) { // 입력받은 수가 0이면
+            throw new ZeroException(); // 커스텀 ZeroException을 throw
         }
-        return n;
+        return n; // 입력받은 정수 리턴
     }
 }
 
 class AddN {
-    GetN gn;
+    GetN gn; // 위에서 선언한 메소드형 지시자 선언
 
     AddN(GetN gn) {
-        this.gn = gn;
+        this.gn = gn; // 입력받은 GetN형 객체를 gn 필드에 집어넣기
     }
 
-    int get_addn() throws IOException, ZeroException, OvfException {
-        int n = gn.get_n();
-        n = n + 1;
-        if (n > 5) {
-            throw new OvfException();
+    int get_addn() throws IOException, ZeroException, OvfException { // 넘겨받은 예외도 catch해주지 않으면 같이 넘겨줄것을 선언해야 한다.
+        int n = gn.get_n(); // 사용자에게 정수 입력받는 메소드 호출
+        n = n + 1; // 그 값에 1을 더하고
+        if (n > 5) { // 그 값의 범위가 0~4 범위이면
+            throw new OvfException(); // 커스텀 OvfException을 throw
         }
-        return n;
+        return n; //
     }
 }
 
 class H1_Exception {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         AddN adn = new AddN(new GetN());
         try {
             int n = adn.get_addn();
@@ -46,7 +45,6 @@ class H1_Exception {
             System.out.println("ZeroException");
         } catch (OvfException e) {
             System.out.println("OvfException");
-        } catch (IOException e) {
         }
     }
 }
