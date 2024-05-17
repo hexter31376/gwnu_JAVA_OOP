2225a486fd110365e5a0fa83ca1118145bcc800d
WonyoungJang <zwonyoung33@gmail.com>
2024-05-18 03:35:20 +0900

    STEP_13_Array_4 create

--- STEP_13_배열과배열리스트/W4_arrayFor/STEP_13_Array_4.java
+++ STEP_13_배열과배열리스트/W4_arrayFor/STEP_13_Array_4.java
@@ -15,17 +15,46 @@ class Student {
 
 class Array_41 {
     void array_test(){
-        
+        int[] data = new int[10];
+
+        System.out.println("Array_41");
+
+        for(int i = 0; i < data.length; i++){
+            data[i] = (int)(Math.random() * 100);
+        }
+        int xa;
+        for(int i = 0; i < data.length; i++){
+            xa = data[i];
+            System.out.printf("%d ", xa);
+        }
+        for(int xb: data){
+            System.out.printf("%d ", xb);
+        }
+        System.out.printf("\n");
     }
 }
 
 class Array_42 {
     void array_test(){
+        Student[] std = new Student[] {
+            new Student("Lee", 18), new Student("Kim", 19),
+            new Student("Park", 20) };
+        System.out.println("Array_42");
 
+        Student sa;
+        for(int i = 0; i < std.length; i++){
+            sa = std[i];
+            System.out.printf("%s ", sa.getName());
+        }
+        System.out.printf("\n");
+
+        for(Student sb: std){
+            System.out.printf("%s ", sb.getName());
+        }
     }
 }
 
-public class STEP_13_Array_3 {
+public class STEP_13_Array_4 {
 
     public static void main(String[] args) {
         new Array_41().array_test();
