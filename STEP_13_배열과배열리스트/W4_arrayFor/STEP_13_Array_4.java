8a73419f1629ad77a3ee96e2be2137f0b637a3c6
WonyoungJang <zwonyoung33@gmail.com>
2024-05-18 03:19:22 +0900

    remmove *.classes and W4 create

---
+++ STEP_13_배열과배열리스트/W4_arrayFor/STEP_13_Array_4.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Student {
+    String name;
+    Integer age;
+
+    Student(String name, Integer age){
+        this.name = name;
+        this.age = age;
+    }
+    String getName(){
+        return (name);
+    }
+}
+
+class Array_41 {
+    void array_test(){
+        
+    }
+}
+
+class Array_42 {
+    void array_test(){
+
+    }
+}
+
+public class STEP_13_Array_3 {
+
+    public static void main(String[] args) {
+        new Array_41().array_test();
+        new Array_42().array_test();
+    }
+}
