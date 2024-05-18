4c34a565dd8f2d0a4d69c28254e2a4fd6b8f9a77
WonyoungJang <zwonyoung33@gmail.com>
2024-05-18 16:04:03 +0900

    add comment

--- STEP_13_배열과배열리스트/W4_arrayFor/STEP_13_Array_4.java
+++ STEP_13_배열과배열리스트/W4_arrayFor/STEP_13_Array_4.java
@@ -13,7 +13,7 @@ class Student { // 학생 클래스 생성
     }
 }
 
-class Array_41 {
+class Array_41 { // 배열 전용 반복문 테스트 클래스
     void array_test(){
         int[] data = new int[10]; // 1차원 배열형 data 객체 지시자를 생성하고 거기에 10크기의 int 배열 동적 할당
         System.out.println("Array_41"); // 클래스 41을 실행했음을 알리는 프린트 구문
@@ -31,26 +31,26 @@ class Array_41 {
         for(int xb: data){ // 출력하는 것은 동일하나 배열 전용 for문을 사용하였다.
             System.out.printf("%d ", xb);
         }
-        System.out.printf("\n");
+        System.out.printf("\n"); // 개행용 프린트 구문
     }
 }
 
-class Array_42 {
+class Array_42 { // 학생 클래스형 클래스 배열 생성 클래스
     void array_test(){
-        Student[] std = new Student[] {
+        Student[] std = new Student[] { // 학생 배열 생성
             new Student("Lee", 18), new Student("Kim", 19),
             new Student("Park", 20) };
         System.out.println("Array_42");
 
-        Student sa;
+        Student sa; // 학생 배열의 요소를 한개 지정한 변수 sa 선언
         for(int i = 0; i < std.length; i++){
-            sa = std[i];
-            System.out.printf("%s ", sa.getName());
+            sa = std[i]; // sa에 배열의 i번째 요소 저장
+            System.out.printf("%s ", sa.getName()); // sa의 getName클래스로 내용 출력
         }
-        System.out.printf("\n");
+        System.out.printf("\n"); // 개행용 프린트 구문
 
-        for(Student sb: std){
-            System.out.printf("%s ", sb.getName());
+        for(Student sb: std){ // 배열 반복문으로 반복
+            System.out.printf("%s ", sb.getName()); // sb의 getName클래스로 내용 출력
         }
     }
 }
