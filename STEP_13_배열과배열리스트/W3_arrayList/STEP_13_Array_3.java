6f71ca0627650cf08e6e779e83681714cb27d0ae
WonyoungJang <zwonyoung33@gmail.com>
2024-05-18 15:53:44 +0900

    add comment

--- STEP_13_배열과배열리스트/W3_arrayList/STEP_13_Array_3.java
+++ STEP_13_배열과배열리스트/W3_arrayList/STEP_13_Array_3.java
@@ -16,15 +16,19 @@ class Student { // 학생 클래스 생성
 class ArrayList_31 { // 정수형 클래스로 배열 리스트 테스트
     void arrayList_test(){
         ArrayList<Integer> data1 = new ArrayList<Integer>();
+
         ArrayList<Integer> data2; // ArrayList<int> data2;
         data2 = new ArrayList<Integer>();
+
         for(int i = 0; i < 9; i++){
             data2.add(i); //data2.add(new Integer(i));
         }
+
         // int i1 = data2.indexOf(new Integer(8)); // 원본 코드
         int i1 = data2.indexOf(Integer.valueOf(8));
         int i2 = data2.indexOf(8);
         int i3 = data2.indexOf(12);
+
         System.out.printf("31 : size %d, i1 = %d, i2 = %d, i3 = %d\n", data2.size(), i1, i2, i3);
     }
 }
@@ -32,6 +36,7 @@ class ArrayList_31 { // 정수형 클래스로 배열 리스트 테스트
 class ArrayList_32 { // 문자열 클래스로 배열 리스트 테스트
     void arrayList_test(){
         ArrayList<String> name = new ArrayList<String>(); // 스트링형 제네릭을 가진 ArrayList 클래스 객체 지시자를 생성하고 거기에 String형 제네릭을 가진 ArrayList를 생성
+
         name.add("Kim");
         name.add("Lee");
         name.add("Park");
@@ -39,6 +44,7 @@ class ArrayList_32 { // 문자열 클래스로 배열 리스트 테스트
         int j1 = name.indexOf(new String("Lee")); // 해당 요소의 처음 위치 반환 // 해당 요소는 처음부터 시작되므로 1 반환
         int j2 = name.indexOf("Lee"); // 해당 요소의 처음 위치 반환 // 해당 요소는 처음부터 시작되므로 1 반환
         int j3 = name.indexOf("Choi"); // 해당 요소의 처음 위치를 반환하지만 Choi는 배열에 들어있지 않다. 따라서 -1이 된다
+
         System.out.printf("32 : size %d, j1 = %d, j2 = %d, j3 = %d\n", name.size(), j1, j2, j3); // 로직 확인용 출력
     }
 }
@@ -46,7 +52,9 @@ class ArrayList_32 { // 문자열 클래스로 배열 리스트 테스트
 class ArrayList_33 {
     void arrayList_test(){
         ArrayList<Student> std = new ArrayList<>(); // 제네릭을 비워두면 컴파일러가 자료형을 판단, 모든 데이터가 들어갈 수 있다.
+        
         Student lee = new Student("Lee", 19); // Student형 클래스를 Lee와 19를 객체 생성자에 넘겨주면서 생성
+
         std.add(new Student("Lee", 19)); // std.add로 데이터 직접 new로 생성하고 할당
         std.add(lee); // 아까 생성한 lee 직접 할당
         std.add(new Student("Kim", 18)); // std.add로 데이터 직접 new로 생성하고 할당
