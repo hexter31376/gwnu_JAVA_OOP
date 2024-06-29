e914b493225919fbcc05aa0fb400fda562029e58
WonyoungJang <zwonyoung33@gmail.com>
2024-06-29 18:34:42 +0900

    add comment

--- STEP_17_표식/W1_declaredMethod/STEP_17_Annotation_1.java
+++ STEP_17_표식/W1_declaredMethod/STEP_17_Annotation_1.java
@@ -25,7 +25,7 @@ class Student { // 학생 클래스
 class Annotation_11 {
     Annotation_11 () {
         Method m1 = null, m2 = null; // 메소드 클래스 객체 지시자 m1과 m2를 선언하고 메소드 객체 지시자를 null로 초기화
-        int age = 0; // 이름 선언 후 0으로 초기화
+        int age = 0; // 나이 변수 선언 후 0으로 초기화
         Student st = new Student("Lee", 19); // 새로운 학생 객체 생성
 
         try { // 예외 캐치하기
@@ -54,40 +54,40 @@ class Annotation_11 {
 
 class Annotation_12 {
     Annotation_12 () {
-        Method m1 = null, m2 = null;
-        int age = 0;
-        Student st = new Student("Lee", 19);
+        Method m1 = null; // 메소드 객체 지시자 m1을 선언하고 그 값을 null로 지정
+        int age = 0; // 나이 변수 선언 후 0으로 지정
+        Student st = new Student("Lee", 19); // 새로운 학생 객체 생성
 
-        Class<? extends Student> sc = st.getClass(); // Student 클래스와 그 후손 클래스를 모두 포함하는 st.getClass
+        Class<? extends Student> sc = st.getClass(); // 클래스 객체 지시자 sc에 Student 클래스와 그 student 클래스를 상속받는 클래스를 모두 포함하여 받아들일 수 있는 제네릭을  st.getClass에 지정해주고 st의 클래스를 받아줍니다. 즉 student 클래스를 받게 됩니다.
         try {
-            if ((m1 = sc.getDeclaredMethod("getAge")) != null) {
-                age = (int)m1.invoke(st);
+            if ((m1 = sc.getDeclaredMethod("getAge")) != null) { // 만약 sc에서 getAge라는 이름의 메소드를 찾을 수 있다면
+                age = (int)m1.invoke(st); // 그 m1 메소드를 invoke()메소드를 통해 호출합니다.
             }
-        } catch (NoSuchMethodException e) {
-        } catch (IllegalAccessException e) {
-        } catch (InvocationTargetException e) {
+        } catch (NoSuchMethodException e) { // NoSuchMethodException 받아주기
+        } catch (IllegalAccessException e) { // IllegalAccessException 받아주기
+        } catch (InvocationTargetException e) { // InvocationTargetException 받아주기
         }
 
-        System.out.println("Annotation_12 : Age = " + age);
+        System.out.println("Annotation_12 : Age = " + age); // age의 값 출력
     }
 }
 
 class Annotation_13 {
     Annotation_13 () {
-        Method m1 = null;
-        int age = 0;
-        Student st = new Student("Lee", 19);
+        Method m1 = null; // 메소드 객체 지시자 m1을 선언하고 그 값을 null로 지정
+        int age = 0; // 나이 변수 선언 후 0으로 지정
+        Student st = new Student("Lee", 19); // 새로운 학생 객체 생성
 
-        Class<Student> sc = Student.class;
+        Class<Student> sc = Student.class; // Class 클래스 객체는 해당 클래스의 .class 필드로부터 얻을 수도 있습니다.
         try {
             m1 = sc.getDeclaredMethod("getAge_new");
             age = Integer.valueOf((String)m1.invoke(st));
-        } catch (NoSuchMethodException e) {
-        } catch (IllegalAccessException e) {
-        } catch (InvocationTargetException e) {
+        } catch (NoSuchMethodException e) { // NoSuchMethodException 받아주기
+        } catch (IllegalAccessException e) { // IllegalAccessException 받아주기
+        } catch (InvocationTargetException e) { // InvocationTargetException 받아주기
         }
 
-        System.out.println("Annotation_13 : Age = " + age);
+        System.out.println("Annotation_13 : Age = " + age); // age의 값 출력
     }
 }
 public class STEP_17_Annotation_1 {
